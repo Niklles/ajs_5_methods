105cc5707d9a3d71ee153d545df2704c5821568f
Niklles <max777944@inbox.ru>
2025-04-16 23:38:06 +0300

    First commmit

--- src/js/Character.js
+++ src/js/Character.js
@@ -1,20 +1,37 @@
 export default class Character {
-    constructor(name, type, attack, defence) {
-      if (typeof (name) !== 'string') {
-        throw new Error('name must be sting');
-      }
-      if (name.length <= 2 || name.length >= 10) {
-        throw new Error('name must be from 2 to 10 characters');
-      }
-      if (!['Bowman', 'Swordsman', 'Magician', 'Daemon', 'Undead', 'Zombie'].includes(type)) {
-        throw new Error('charecter must be one of default types: Bowman, Swordsman, Magician, Daemon, Undead, Zombie');
-      }
-  
-      this.name = name;
-      this.type = type;
-      this.health = 100;
-      this.level = 1;
-      this.attack = attack;
-      this.defence = defence;
+  constructor(name, type, attack, defence) {
+    if (typeof (name) !== 'string') {
+      throw new Error('name must be sting');
+    }
+    if (name.length <= 2 || name.length >= 10) {
+      throw new Error('name must be from 2 to 10 characters');
+    }
+    if (!['Bowman', 'Swordsman', 'Magician', 'Daemon', 'Undead', 'Zombie'].includes(type)) {
+      throw new Error('charecter must be one of default types: Bowman, Swordsman, Magician, Daemon, Undead, Zombie');
+    }
+
+    this.name = name;
+    this.type = type;
+    this.health = 100;
+    this.level = 1;
+    this.attack = attack;
+    this.defence = defence;
+  }
+
+  levelUp() {
+    if (this.health === 0) {
+      throw new Error('нельзя повысить уровень умершего');
+    }
+    this.level += 1;
+    this.attack *= 1.2;
+    this.defence *= 1.2;
+    this.health = 100;
+  }
+
+  damage(points) {
+    this.health -= points * (1 - this.defence / 100);
+    if (this.health < 0) {
+      this.health = 0;
     }
   }
+}
